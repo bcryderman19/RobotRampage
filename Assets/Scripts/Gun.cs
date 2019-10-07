e9751da58903b747ba6d9918f3ca6cc40f2e1768
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-10-07 16:07:23 -0400

    managing ammunition

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,6 +6,11 @@ public class Gun : MonoBehaviour
 {
     public float fireRate;
 
+    public Ammo ammo;
+
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+
     protected float lastFireTime;
 
     // Start is called before the first frame update
@@ -22,6 +27,15 @@ public class Gun : MonoBehaviour
 
     protected void Fire()
     {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
         GetComponentInChildren<Animator>().Play("Fire");
     }
 }
