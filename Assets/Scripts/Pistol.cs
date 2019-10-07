ccc75ec30ba16f8501b045df51f444bf9bb4eeed
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-10-07 15:08:41 -0400

    ammunition logic

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+    protected override void Update()
+    {
+        base.Update();
+        //Shotgun & Pistol have semi-auto fire rate
+        if (Input.GetMouseButtonDown(0) && (Time.time - lastFireTime) > fireRate)
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
