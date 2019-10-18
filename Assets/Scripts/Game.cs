ded83e0947c9aec5c2561adbb284392da99fb411
Brittany Cryderman [Student] <BCryderman19@Student.SL.On.Ca>
2019-10-18 14:21:08 -0400

    robot spawns

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;
+
+    public int enemiesLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+
+    public void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
